4587a121057a2ce9e9873f8ea5c76b1849b3d40d
Karlen <k112-117@yandex.ru>
2019-04-09 18:17:11 +0300

    Это мой первый комит

---
+++ Lesson0.js
@@ -0,0 +1,61 @@
+'use strict';
+
+//Первый код с udemy
+
+var leftBorderWidth = 1;
+
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+console.log(leftBorderWidth);
+
+
+var number = 5;
+var string = "Hello wordld";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var object = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+alert("Hello world");
+
+
+
+let bool = +prompt("Есть ли Вам 18 лет?", "");
+console.log(bool);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
+
+
+
+
